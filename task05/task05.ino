d558e4d769f87d22391d286d09e3b741452b54a1
tchernets-maria <79788484+tchernets-maria@users.noreply.github.com>
2021-12-02 12:46:49 +0400

    Update task05.ino

--- task05/task05.ino
+++ task05/task05.ino
@@ -1,7 +1,23 @@
+#include <AccelStepper.h>
+
+#define stepPin_1 (2)
+#define dirPin_1 (5)
+          
+#define stepPin_2 (3)
+#define dirPin_2 (6)
+
+AccelStepper stepper1(1, stepPin_1, dirPin_1);
+AccelStepper stepper2(1, stepPin_2, dirPin_2);
+ 
 void setup() {
-
+  stepper1.setMaxSpeed(500.0);
+  stepper1.setSpeed(25.0);
+ 
+  stepper2.setMaxSpeed(400.0);
+  stepper2.setSpeed(80.0);
 }
-
+ 
 void loop() {
-
+  stepper1.runSpeed();
+  stepper2.runSpeed();
 }
